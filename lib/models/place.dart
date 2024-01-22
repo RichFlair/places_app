7fd1148459586825fe4c1cc1dfa6a01efdb97f42
Andrew Marfo <andrewmarfo23@gmail.com>
2024-01-22 00:07:32 +0000

    place class

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,10 +1,27 @@
 import 'dart:io';
 
+class PlaceLocation {
+  final double latittude;
+  final double longitude;
+  final String? address;
+
+  PlaceLocation({
+    required this.latittude,
+    required this.longitude,
+    this.address,
+  });
+}
+
 class Place {
   final String id;
   final String title;
-  final ... location;
+  final PlaceLocation location;
   final File image;
 
-  
+  Place({
+    required this.id,
+    required this.title,
+    required this.location,
+    required this.image,
+  });
 }
