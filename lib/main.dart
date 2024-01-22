cc21a4cd8fe2346e8f05a4a0a6c042be868feee0
Andrew Marfo <andrewmarfo23@gmail.com>
2024-01-22 00:26:01 +0000

    integrating provider in the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/places_list_screen.dart';
+import './providers/great_places_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider.value(
+      value: GreatPlacesProvider(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const PlacesListScreen(),
       ),
-      home: const PlacesListScreen(),
     );
   }
 }
