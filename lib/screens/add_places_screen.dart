371812540c80301597ba090715be08be4bc88b22
Andrew Marfo <andrewmarfo23@gmail.com>
2024-01-23 18:43:29 +0000

    building the add places screen

--- lib/screens/add_places_screen.dart
+++ lib/screens/add_places_screen.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class AddPlacesScreen extends StatefulWidget {
+  const AddPlacesScreen({super.key});
+
+  @override
+  State<AddPlacesScreen> createState() => _AddPlacesScreenState();
+}
+
+class _AddPlacesScreenState extends State<AddPlacesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Add Place',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
